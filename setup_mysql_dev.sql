5fb1a8c718abd79791de7d6e8341a62f88bd2d45
webstafrank <franklyndenish@gmail.com>
2024-06-26 07:57:49 -0700

    MYSQL setup development

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+--create  a database if it aint there
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it isin't
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on hbnb_dev_db to hbnb_dev
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to hbnb_dev
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Flush privileges to ensure they are applied
+FLUSH PRIVILEGES;

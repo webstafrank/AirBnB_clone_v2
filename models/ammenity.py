279bccb2851d40c2b5c44322e0d29ae10fa8b767
webstafrank <franklyndenish@gmail.com>
2024-06-26 10:21:44 -0700

    DB storage ammenity

---
+++ models/ammenity.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+class Amenity(BaseModel, Base):
+    __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
+
+    # Relationships
+    place_amenities = relationship('Place', secondary='place_amenity', back_populates='amenities')
